0a725e372331cb6edc0794ede7554261e4e49135
ildgud <rubgavrilov@gmail.com>
2020-10-20 16:38:54 +0700

    First commit

---
+++ modul01/lesson01_introduction/src/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Hello! My name is ...");
+        }
+        System.out.println();
+        for (int j = 0; j < 5; j++) {
+            System.out.println("I want to be a java developer!");
+        }
+    }
+}
